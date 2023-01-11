f9e40800e5daf9f76e37c87296694738008634bf
Cremheda1973 <85742360+LearningCodePython@users.noreply.github.com>
2023-01-11 14:09:25 +0000

    Update api_gettoken.py
    Depurando

--- api/api_gettoken.py
+++ api/api_gettoken.py
@@ -1,10 +1,4 @@
 # by : Cristo Emiliano Hernandez Darias 
-#
-## Repo API_Yeastar in 10.19.19.16/BonoboGit
-## Nueva rama para hacer pruebas con la central de Guayarmina 10.102.0.10
-
-## take_toke.py se concecta con la central y almacena en la variable tocken el valor de token necesario
-## para posteriormente hacer el POST a la central, necesario para interectuar con la API de la Yeastar
 
 import requests
 from requests.models import Response
