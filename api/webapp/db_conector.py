ec07555715fcd75818a8ad9105825917cae73dd9
Emilio Hernández <emilio.hernandez.darias@gmail.com>
2024-02-11 20:26:12 +0000

    Etiquetando partes del codigo de db_conector.py

--- api/webapp/db_conector.py
+++ api/webapp/db_conector.py
@@ -1,5 +1,7 @@
 import mysql.connector
 
+# Conectos usado posteriormente para acceder a la base de datos
+# No se selecciona base de datos
 mydb = mysql.connector.connect(
 	 	host="localhost",
 		user="root",
